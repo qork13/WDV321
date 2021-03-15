293a91cf60e9efa4990efce1728f0bd42e05e3b8
qork13 <57408249+qork13@users.noreply.github.com>
2021-03-15 18:48:33 -0500

    Initial JSON project files

---
+++ createJSON.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+/*
+	This file will:
+	
+	- Create a Javascript object including an array
+	- Convert the Javascript object into a JSON object
+	- Store the JSON object into local storage
+	
+	Goal: Provide an example of how to create JSON objects in Javascript
+	Goal: Provide an example of how to consume JSON objects in Javascript
+
+	Use the following data for your JSON object
+
+		student_id = 332443
+		student_gpa = 3.6
+		student_courses = ["WDV101","WDV131","WDV105"]
+
+		student_id = 545467
+		student_gpa = 2.7
+		student_courses = ["WDV101","WDV131","WDV105","WDV221","WDV205"]	
+		
+		student_id = 128574
+		student_gpa = 3.4
+		student_courses = ["WDV101","WDV131","WDV105","WDV221","WDV205","WDV341"]	
+
+
+
+*/
+
---
+++ createJSONArray.js
@@ -0,0 +1,34 @@
+// JavaScript Document
+/*
+	This file will:
+	
+	- Create a Javascript object containing an array of records with each record including an array
+	- Convert the Javascript object into a JSON object
+	- Store the JSON object into local storage
+	
+	This will be given to students to run as part of a web page that will consume the JSON object
+	Goal: Provide an example of how to create a JSON object in JS
+	Goal: Provide an example of how to consume a JSON object in JS
+
+	Use the following data for this object:
+	
+		student_id = 332443
+		student_gpa = 3.6
+		student_courses = ["WDV101","WDV131","WDV105"]	
+
+		student_id = 545467
+		student_gpa = 2.7
+		student_courses = ["WDV101","WDV131","WDV105","WDV221","WDV205"]	
+		
+		student_id = 128574
+		student_gpa = 3.4
+		student_courses = ["WDV101","WDV131","WDV105","WDV221","WDV205","WDV341"]	
+	
+		student_id = 750056
+		student_gpa = 1.85
+		student_courses = ["WDV101","WDV131","WDV105","WDV221","WDV205"]		
+		
+		
+*/
+
+
