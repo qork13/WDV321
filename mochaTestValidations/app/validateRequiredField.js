c519f2c5da83839d0a4dac3761fff0c8750f8e60
qork13 <57408249+qork13@users.noreply.github.com>
2021-04-08 18:05:51 -0500

    Update validateRequiredField.js
    Update Test plan

--- mochaTestValidations/app/validateRequiredField.js
+++ mochaTestValidations/app/validateRequiredField.js
@@ -1,14 +1,15 @@
 // JavaScript Document
 var validatePhoneNumber = require('../app/validatePhoneNumber');
 /*
-	1.The letter a should pass
-	2.The number 4 should pass
-	3.Empty or '' should fail
-	4.A single space should fail
-	5.Two or more spaces should fail
-	6.The word null should fail
-	7.The word undefined should fail
-	8.The value 'a 4' should pass
+	Input		Expected		Actual
+	a			true			true
+	4			true			true
+				false			false
+	 			false			false
+	  			false			false
+	null		false			false
+	undefined	false			false
+	a 4			true			true
 
 */
 var validInput = function(inValue){
